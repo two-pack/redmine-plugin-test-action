386c56736ad0eab4747126001d85fdd2d100a29e
twopack <twopackas@gmail.com>
2020-01-08 21:55:04 +0900

    v2: Changes the action using Docker for performance improvement.

---
+++ lib/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo Start redmine-plugin-test-action
+
+echo Set environment variables
+set -xe
+PLUGIN_REPO=https://github.com/${GITHUB_REPOSITORY}.git
+PLUGIN_BRANCH=$(echo ${GITHUB_REF#refs/heads/})
+WORKSPACE=/var/lib/redmine
+TRACE=--trace
+
+echo Install plguin
+cd ${WORKSPACE}
+git clone -b ${PLUGIN_BRANCH} --depth 1 ${PLUGIN_REPO} ${WORKSPACE}/plugins/${PLUGIN_NAME}
+bundle install --path vendor/bundle
+bundle exec rake redmine:plugins:migrate $TRACE
+
+echo Run tests
+RAILS_ENV=test RUBYOPT=-W0 TESTOPTS=-v bundle exec rake $TRACE redmine:plugins:test NAME=${PLUGIN_NAME}
+
+echo Uninstall plugin
+bundle exec rake $TRACE redmine:plugins:migrate NAME=${PLUGIN_NAME} VERSION=0
+
+echo Finish redmine-plugin-test-action
--- lib/redmine-plugin-test.sh
+++ lib/redmine-plugin-test.sh
@@ -1,39 +1,15 @@
 #!/bin/bash
 
-echo Start redmine-plugin-test-action
-
-echo Set environment variables
 set -xe
-PLUGIN=$1
-REDMINE_VERSION=$2
-REDMINE_GIT_REPO=$3
-WORKSPACE=$(cd $GITHUB_WORKSPACE/../.. && pwd)/workspace
-PATH_TO_PLUGIN=$GITHUB_WORKSPACE
-PATH_TO_REDMINE=$WORKSPACE/redmine
-PATH_TO_PLUGINS=$PATH_TO_REDMINE/plugins/$PLUGIN
-PATH_DBYML=$(cd $(dirname $0)/../config && pwd)/database.yml
-TRACE=--trace
+PLUGIN_NAME=$1
+REDMINE_VERSION=(${2/v/})
+RUBY_VERSION=(${3/v/})
 
-echo Install Redmine
-mkdir $WORKSPACE
-rm -rf $PATH_TO_REDMINE
-git clone -b $REDMINE_VERSION $REDMINE_GIT_REPO $PATH_TO_REDMINE
-cd $PATH_TO_REDMINE
-
-echo Install plguin
-ln -s $PATH_TO_PLUGIN $PATH_TO_PLUGINS
-cp $PATH_DBYML config/database.yml
-mkdir -p vendor/bundle
-bundle install --path vendor/bundle
-bundle exec rake db:migrate $TRACE
-bundle exec rake redmine:load_default_data REDMINE_LANG=en $TRACE
-bundle exec rake generate_secret_token $TRACE
-bundle exec rake redmine:plugins:migrate $TRACE
-
-echo Run tests
-RAILS_ENV=test RUBYOPT=-W0 TESTOPTS=-v bundle exec rake $TRACE redmine:plugins:test NAME=$PLUGIN
-
-echo Uninstall plugin
-bundle exec rake $TRACE redmine:plugins:migrate NAME=$PLUGIN VERSION=0
-
-echo Finish redmine-plugin-test-action
+cd $(cd $(dirname $0)/../ && pwd)
+docker build -t redmine-plugin-test \
+             --build-arg REDMINE_VERSION=${REDMINE_VERSION} \
+             --build-arg RUBY_VERSION=${RUBY_VERSION} .
+docker run -e "GITHUB_REF=${GITHUB_REF}" \
+           -e "GITHUB_REPOSITORY=${GITHUB_REPOSITORY}" \
+           -e "PLUGIN_NAME=${PLUGIN_NAME}" \
+           -t redmine-plugin-test
